f7a4f1d81afe9ec5e2863302f7e4fda713160a54
Priyanshu <sidarsunita2@gmail.com>
2025-10-20 07:58:05 +0530

    changel

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send({ message: "Hi there" });
+  res.send({ message: "Hi there user" });
 });
 
 app.post("/signup", async (req, res) => {
